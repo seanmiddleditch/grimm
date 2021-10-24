79f6911d02edba799bff46571674af39c7603b1a
Sean Middleditch <sean.middleditch@gmail.com>
2021-10-24 11:26:56 -0700

    Linux build fix

--- potato/libruntime/source/callstack.posix.cpp
+++ potato/libruntime/source/callstack.posix.cpp
@@ -33,7 +33,11 @@ auto up::callstack::resolveTraceRecords(span<uintptr const> addresses, span<Trac
     for (auto index : sequence(max)) {
         auto& record = records[index];
         record.address = addresses[index];
-        record.symbol = string_view(symbols[index]);
+
+        *nanofmt::copy_to(
+            record.symbol,
+            record.symbol + sizeof record.symbol - 1,
+            symbols[index]) = '\0';
     }
 
     free(symbols); // NOLINT(cppcoreguidelines-no-malloc)
