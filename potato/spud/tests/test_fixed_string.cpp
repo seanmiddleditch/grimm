5c9b687d3ca6039cbc23e857cf112e68f6d83730
Sean Middleditch <sean.middleditch@gmail.com>
2021-10-24 19:45:08 -0700

    Fix fixed_string hashing

--- potato/spud/tests/test_fixed_string.cpp
+++ potato/spud/tests/test_fixed_string.cpp
@@ -2,6 +2,7 @@
 
 #include "potato/spud/fixed_string.h"
 #include "potato/spud/string_view.h"
+#include "potato/spud/hash.h"
 
 #include <catch2/catch.hpp>
 #include <iostream>
@@ -36,5 +37,16 @@ TEST_CASE("potato.spud.fixed_string", "[potato][spud]") {
         constexpr fixed_string test3 = "This is different";
         CHECK(test1 == test2);
         CHECK(test1 != test3);
+    }
+
+    SECTION("hash fixed_string") {
+        auto const fhash = hash_value(fixed_string{"hash this"});
+        auto const shash = hash_value("hash this");
+        auto const vhash = hash_value(string_view{"hash this"});
+        auto const whash = hash_value(fixed_string{"hash this different thing"});
+
+        CHECK(fhash == shash);
+        CHECK(fhash == vhash);
+        CHECK(whash != fhash);
     }
 }
