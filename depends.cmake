78e2a197d67b64d4f41adf371b383b7d5d54bcfb
Sean Middleditch <sean.middleditch@gmail.com>
2022-04-01 22:44:14 -0700

    Update git repository URLs
    See https://github.blog/2021-09-01-improving-git-protocol-security-github/

--- depends.cmake
+++ depends.cmake
@@ -2,7 +2,7 @@ include(up_require)
 include(FetchContent)
 
 up_require(libbullet3_cmake
-    GIT_REPOSITORY git://github.com/potatoengine/libbullet3-cmake.git
+    GIT_REPOSITORY https://github.com/potatoengine/libbullet3-cmake.git
     GIT_COMMIT f27bf2d0d0d50051df8380b45d0b000b0ab9056a
 )
 up_require(sqlite_source
@@ -11,29 +11,29 @@ up_require(sqlite_source
     CMAKE_FILE sqlite.cmake
 )
 up_require(catch2
-    GIT_REPOSITORY git://github.com/catchorg/Catch2.git
+    GIT_REPOSITORY https://github.com/catchorg/Catch2.git
     GIT_COMMIT c4e3767e265808590986d5db6ca1b5532a7f3d13
     GIT_TAG v2.13.7
     CMAKE_FILE catch2.cmake
 )
 up_require(imgui
-    GIT_REPOSITORY git://github.com/rokups/imgui.git # temporary branch until upstream is fixed
-    GIT_COMMIT ce92c1b17ca58201522954a0662a91b2a2556a19 # rk/fix-5012-again
+    GIT_REPOSITORY https://github.com/ocornut/imgui.git
+    GIT_COMMIT 9b0c26b0b2adae3ccf66dc9552fae4945d735a0c
     CMAKE_FILE imgui.cmake
 )
 up_require(glm
-    GIT_REPOSITORY git://github.com/g-truc/glm.git
+    GIT_REPOSITORY https://github.com/g-truc/glm.git
     GIT_COMMIT bf71a834948186f4097caa076cd2663c69a10e1e
     GIT_TAG 0.9.9.8
     CMAKE_FILE glm.cmake
 )
 up_require(stb
-    GIT_REPOSITORY git://github.com/nothings/stb.git
+    GIT_REPOSITORY https://github.com/nothings/stb.git
     GIT_COMMIT af1a5bc352164740c1cc1354942b1c6b72eacb8a
     CMAKE_FILE stb.cmake
 )
 up_require(json
-    GIT_REPOSITORY git://github.com/nlohmann/json.git
+    GIT_REPOSITORY https://github.com/nlohmann/json.git
     GIT_COMMIT aa0e847e5b57a00696bdcb6a834b927231b81613
     GIT_TAG v3.10.3
     CMAKE_FILE json.cmake
@@ -49,41 +49,41 @@ up_require(assimp_win64_sdk
     CMAKE_FILE assimp_win64.cmake
 )
 up_require(flatbuffers
-    GIT_REPOSITORY git://github.com/google/flatbuffers.git
+    GIT_REPOSITORY https://github.com/google/flatbuffers.git
     GIT_COMMIT 697147a2e686486424b9d15fc3e1612586a60f97
     GIT_TAG v1.12.1
     CMAKE_FILE flatbuffers.cmake
 )
 up_require(soloud
-    GIT_REPOSITORY git://github.com/jarikomppa/soloud.git
+    GIT_REPOSITORY https://github.com/jarikomppa/soloud.git
     GIT_COMMIT c8e339fdce5c7107bdb3e64bbf707c8fd3449beb
     GIT_TAG RELEASE_20200207
     CMAKE_FILE soloud.cmake
 )
 up_require(nfd 
-    GIT_REPOSITORY git://github.com/mlabbe/nativefiledialog.git
+    GIT_REPOSITORY https://github.com/mlabbe/nativefiledialog.git
     GIT_COMMIT 67345b80ebb429ecc2aeda94c478b3bcc5f7888e
     GIT_TAG release_116
     CMAKE_FILE nfd.cmake
 )
 up_require(sapc
-    GIT_REPOSITORY git://github.com/potatoengine/sapc.git
+    GIT_REPOSITORY https://github.com/potatoengine/sapc.git
     GIT_COMMIT cfc3bb6d44e67d87d38d6c7406710bd26c1e2cb3
 )
 up_require(tracy
-    GIT_REPOSITORY git://github.com/wolfpld/tracy.git
+    GIT_REPOSITORY https://github.com/wolfpld/tracy.git
     GIT_COMMIT 07778badcced109b8190805fbf2d7abfaef0d3b9
     GIT_TAG v0.7.8
     CMAKE_FILE tracy.cmake
 )
 up_require(libuv
-    GIT_REPOSITORY git://github.com/libuv/libuv.git
+    GIT_REPOSITORY https://github.com/libuv/libuv.git
     GIT_COMMIT 1dff88e5161cba5c59276d2070d2e304e4dcb242
     GIT_TAG v1.41.0
     CMAKE_FILE libuv.cmake
 )
 up_require(nanofmt
-    GIT_REPOSITORY git://github.com/seanmiddleditch/nanofmt.git
+    GIT_REPOSITORY https://github.com/seanmiddleditch/nanofmt.git
     GIT_COMMIT b979434328e1f0ec5e6ef977bc59a251c4bc9dfc
 )
 if(UP_BUILD_D3D12)
